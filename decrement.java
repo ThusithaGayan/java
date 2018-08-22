f5f07bed443b6b81be1cfc04c66cd2c2b37939a5
ThusithaGayan <39271962+ThusithaGayan@users.noreply.github.com>
2018-08-22 19:25:55 +0530

    Add files via upload

---
+++ decrement.java
@@ -0,0 +1,19 @@
+class decrement{
+	public static void main(String args[]){
+		int x,y,a,b;
+		a=10;
+		b=10;
+
+		x=a--;
+		y=--b;
+
+		//new value of a
+		System.out.println("a="+a);
+		//new value of x
+		System.out.println("x="+x);
+		//new value of b
+		System.out.println("b="+b);
+		//value of y
+		System.out.println("y="+y);
+	}
+}
