989f95804db5ee43d5bfe8bd59e90d3c7e813812
ThusithaGayan <39271962+ThusithaGayan@users.noreply.github.com>
2018-08-23 00:06:04 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,14 @@
+class A{
+	 
+	public static void main(String args[]){
+		int a,b;
+		a=10;
+		b=3;
+
+		System.out.println("a/b="+a/b);
+
+		//type casting (data_type)variable
+		System.out.println("a/b="+(double)a/(double)b);
+		
+	}
+}
