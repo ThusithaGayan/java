68d46f7002e0cca21c10d89b228aa3e634d1b1b8
ThusithaGayan <39271962+ThusithaGayan@users.noreply.github.com>
2018-08-22 15:24:44 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,17 @@
+class variable{
+	
+
+	public static void main(String args[]){
+		String name;
+		name="thusitha";
+		int age=23;
+		char gender='m';
+		//comment
+		/*comment...........................................................................
+		.......................*/
+		System.out.println("Name "+name);
+		System.out.println("Age "+age);
+		System.out.println("Gender "+gender);
+
+	}
+}
