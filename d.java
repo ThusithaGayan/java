9bb67d81eb040a478b9317c61ec258d14d7e1a8d
ThusithaGayan <39271962+ThusithaGayan@users.noreply.github.com>
2018-08-23 10:04:14 +0530

    Add files via upload

---
+++ d.java
@@ -0,0 +1,12 @@
+class d{
+	public static void main(String args[]){
+		for(int i=1;i<=5;i++){
+			System.out.println(i);
+			if(i==3){
+				break;
+				//jumps out of a loop and effectively bypass the loop condition
+			}
+
+		}
+	}
+}
