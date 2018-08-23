3eef3301dbee4fd7ebdca88521e15e3e6d428edc
ThusithaGayan <39271962+ThusithaGayan@users.noreply.github.com>
2018-08-23 09:33:40 +0530

    Add files via upload

---
+++ cat.java
@@ -0,0 +1,13 @@
+class cat{
+	public static void main(String args[]){
+		int i=4;
+		do{
+			//check while(boolean expression) after the do{statement}
+			//The enclosed Statements will be executed at least once irrespective of the Boolean expressions result
+			System.out.println(i);
+			i--;
+		}while(i>5);
+
+	}
+
+}
